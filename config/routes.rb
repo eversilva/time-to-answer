e4bac40633c0ee0c277daaaeae4cf164018d6acd
EverSilva <everson.silva2706@gmail.com>
2023-06-18 19:22:07 -0300

    feat: set welcome to root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'inicio' to: 'welcome#index'
+
+  root to: 'welcome#index'
 end
