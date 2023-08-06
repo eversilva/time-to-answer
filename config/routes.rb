502e1cf803055d0104a21ace6a81cb599b424161
EverSilva <everson.silva2706@gmail.com>
2023-08-06 14:43:44 -0300

    feat: migration to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_for :admins
+
   namespace :website do
     get 'welcome/index'
   end
-  namespace :profiles_backoffice do
+  namespace :users_backoffice do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
     get 'welcome/index'
   end
-  devise_for :admins
 
   get 'inicio', to: 'website/welcome#index'
 
