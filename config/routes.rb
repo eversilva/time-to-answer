75d4100aa64b7108595b31ca8b5632fe73d96a09
EverSilva <everson.silva2706@gmail.com>
2023-08-13 21:02:19 -0300

    refactor: use resources to renders routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  namespace :admins_backoffice do
-    get 'admins/index'
-  end
+
   devise_for :users
   devise_for :admins
 
@@ -13,6 +11,7 @@ Rails.application.routes.draw do
   end
   namespace :admins_backoffice do
     get 'welcome/index'
+    resources :admins, only: [:index]
   end
 
   get 'inicio', to: 'website/welcome#index'
