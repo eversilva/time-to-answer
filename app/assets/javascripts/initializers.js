4e6c231b0ae393f30840e95143e610ef823d2847
EverSilva <everson.silva2706@gmail.com>
2023-08-14 23:42:40 -0300

    feat: add toast message on error

--- app/assets/javascripts/initializers.js
+++ app/assets/javascripts/initializers.js
@@ -1,2 +1,9 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const toastList = document.querySelectorAll('.toast')
+toastList.forEach(toastElement => {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastElement)
+    toastBootstrap.show()
+    
+})
