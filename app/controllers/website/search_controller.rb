50db7f1cd62de80140e8880f8fd80006bff27a1d
EverSilva <everson.silva2706@gmail.com>
2023-09-05 20:16:12 -0300

    refactor: search model class to question

--- app/controllers/website/search_controller.rb
+++ app/controllers/website/search_controller.rb
@@ -1,8 +1,5 @@
 class Website::SearchController < WebsiteController
     def questions
-        @questions = Question.includes(:answers)
-                            .where('lower(description) LIKE ?', "%#{params[:term].downcase}%")
-                            .page(params[:page])
-                            .per(10)
+        @questions = Question.search(params[:term], params[:page])
     end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,11 @@ class Question < ApplicationRecord
   belongs_to :subject, inverse_of: :questions
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
+
+  def self.search(term, page, per = 10)
+    Question.includes(:answers)
+            .where('lower(description) LIKE ?', "%#{term.downcase}%")
+            .page(page)
+            .per(per)
+  end
 end
