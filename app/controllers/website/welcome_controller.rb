35e5685bf1fa4684f2ed65d99a10d1cd6c49dd66
EverSilva <everson.silva2706@gmail.com>
2023-09-05 20:31:10 -0300

    feat: order questions in desc by created at

--- app/controllers/website/welcome_controller.rb
+++ app/controllers/website/welcome_controller.rb
@@ -1,5 +1,5 @@
 class Website::WelcomeController < WebsiteController
   def index
-    @questions = Question.includes(:answers).page(params[:page]).per(10)
+    @questions = Question.includes(:answers).order('created_at desc').page(params[:page]).per(10)
   end
 end
