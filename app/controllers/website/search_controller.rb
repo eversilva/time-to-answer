6499d7f533992add463c6e4563c4e046e54fe297
EverSilva <everson.silva2706@gmail.com>
2023-09-05 20:01:07 -0300

    feat: add pagination to search term result

--- app/controllers/website/search_controller.rb
+++ app/controllers/website/search_controller.rb
@@ -2,5 +2,7 @@ class Website::SearchController < WebsiteController
     def questions
         @questions = Question.includes(:answers)
                             .where('lower(description) LIKE ?', "%#{params[:term].downcase}%")
+                            .page(params[:page])
+                            .per(10)
     end
 end
